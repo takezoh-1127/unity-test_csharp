754444d70a61d5d05f1013e571857a0e15089c3e
takezoh <takezoh@hogehoge.co.jp>
2017-04-24 19:38:17 +0900

    配列、可変長配列、連想配列のテスト

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿#define _ENABLE_HOGE
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//#define _ENABLE_HOGE		// ファイルの先頭で宣言しないとエラーになる.
 
 public class Main : MonoBehaviour {
 
@@ -57,6 +61,176 @@ public class Main : MonoBehaviour {
 			Debug.LogError("### error!!.");
 
 			Debug.Log("### check:0");
+
+#if _ENABLE_HOGE
+			Debug.Log("### #if _ENABLE_HOGE");
+#endif
+
+//#if defined(_ENABLE_HOGE)
+			//Debug.Log("### #if defined(_ENABLE_HOGE)");
+//#endif
+		}
+
+		// 配列.
+		{
+			// ↓NG
+			// C,C++の様な書き方はダメ.
+			//int array[10];
+
+			int[] array = new int[10];
+
+			array[0] = 1;
+
+			Debug.Log("### Array.");
+
+			for(int i = 0; i < array.Length; i++)
+			{
+				Debug.LogFormat("  [{0}]:{1}", i, array[i]);
+			}
+		}
+
+		{
+			int[] array = new int[10]{1,2,3,4,5,6,7,8,9,10};
+
+			// foreachの書き方.
+			foreach(int v in array)
+			{
+				Debug.LogFormat("  {0}", v);
+			}
+		}
+
+		{
+			int[] array = new int[] { 10, 20, 30, 40, 50 };
+
+			// auto変数みたいにvarだと型を明示的に指定する必要が無い？.
+			foreach(var v in array)
+			{
+				Debug.LogFormat("  {0}", v);
+			}
+		}
+
+		{
+			float[] array = new float[] { 1.5f, 2.5f, 3.5f, 4.5f, 5.5f };
+
+			foreach(var v in array)
+			{
+				Debug.LogFormat("  {0}", v);
+			}
+		}
+
+		// 可変長配列.
+		{
+			Debug.Log("### List<int>");
+
+			List<int> list = new List<int>();
+
+			// 追加.
+			list.Add(10);
+			list.Add(20);
+			list.Add(30);
+			list.Add(40);
+
+			Debug.LogFormat("  count:[{0}]", list.Count);
+
+			// 配列の様にアクセスできるみたい.
+			for(int i = 0; i < list.Count; i++)
+			{
+				Debug.LogFormat("  [{0}]:[{1}]", i, list[i]);
+			}
+
+			// 挿入.
+			list.Insert(2, 100);
+
+			Debug.LogFormat("  count:[{0}]", list.Count);
+
+			foreach(var v in list)
+			{
+				Debug.LogFormat("  [{0}]", v);
+			}
+
+			// ソート.
+			Debug.Log("### sort.");
+
+			list.Sort();
+
+			foreach(var v in list)
+			{
+				Debug.LogFormat("  [{0}]", v);
+			}
+
+			// 降順にソート.
+			// ラムダ式.
+			list.Sort((a, b) => b - a);
+
+			foreach (var v in list)
+			{
+				Debug.LogFormat("  [{0}]", v);
+			}
+		}
+
+		{
+			Debug.Log("### List<float>");
+
+			List<float> list = new List<float>();
+
+			list.Add(2.5f);
+			list.Add(2.6f);
+			list.Add(2.7f);
+			list.Add(4.5f);
+			list.Add(6.5f);
+			list.Add(8.5f);
+
+			list.Insert(2, 10.5f);
+
+			foreach (var v in list)
+			{
+				Debug.LogFormat("  [{0}]", v);
+			}
+
+			list.Sort();
+			list.Reverse();
+
+			Debug.Log("##");
+
+			//list.Sort((a, b) => { return a < b ? 1 : (b > a ? -1 : 0); });
+			//list.Sort((a, b) => (int)(b - a));
+
+			foreach (var v in list)
+			{
+				Debug.LogFormat("  [{0}]", v);
+			}
+		}
+
+		// 連想配列.
+		{
+			Debug.Log("### Dictionary<int, string>");
+
+			Dictionary<int, string> dic = new Dictionary<int, string>();
+
+			dic.Add(1, "Hoge1");
+			dic.Add(2, "Hoge2");
+
+			dic[3] = "Hoge3";
+
+			Debug.LogFormat("  val:[{0}]", dic[1]);
+
+			// キーの列挙.
+			foreach(var key in dic.Keys)
+			{
+				Debug.LogFormat("  key:[{0}]", key);
+			}
+
+			// 要素の列挙.
+			foreach(var val in dic.Values)
+			{
+				Debug.LogFormat("  val:[{0}]", val);
+			}
+
+			// キーと要素の列挙.
+			foreach(KeyValuePair<int,string> pair in dic)
+			{
+				Debug.LogFormat("  [{0}] : [{1}]", pair.Key, pair.Value);
+			}
 		}
 	}
 	
